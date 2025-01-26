d038048669190ae6567d534a1d97dc2be53babab
Ho Chun Lau <39511811+kylelau519@users.noreply.github.com>
2025-01-25 16:57:06 -0800

    dynamically linked with database through PDGDB_PATH in .env inside the cargo

--- src/pdgdb/connection.rs
+++ src/pdgdb/connection.rs
@@ -1,9 +1,29 @@
 use rusqlite::Connection;
-use dotenv::dotenv;
+use dotenv::{dotenv, from_path};
 
 pub fn connect() -> Result<Connection, Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let pdgdb = std::env::var("PDGDB_PATH").unwrap_or_else(|_| "./pdg-2024-v0.1.3.sqlite".to_string());
+    let cargo_dir = env!("CARGO_MANIFEST_DIR");
+    let dotenv_path = format!("{}/.env", cargo_dir);
+    from_path(&dotenv_path).ok();
+
+    let pdgdb = std::env::var("PDGDB_PATH").unwrap();
+    println!("{}",&pdgdb);
     let conn = Connection::open(pdgdb)?;
     Ok(conn)
+}
+
+#[cfg(test)]
+mod test{
+
+    use super::*;
+    #[test]
+    fn test_db_env_path()-> Result<(), Box<dyn std::error::Error>>{
+        let cargo_dir = env!("CARGO_MANIFEST_DIR");
+        let dotenv_path = format!("{}/.env", cargo_dir);
+        from_path(&dotenv_path).ok();
+        dbg!(std::env::vars());
+        let pdgpath = std::env::var("PDGDB_PATH").unwrap();
+        assert_eq!(pdgpath, "/Users/kylelau519/Programming/pdgQuery/pdg-2024-v0.1.3.sqlite");
+        Ok(())
+    }
 }
